71f41cc34ad4231432fbe4d5ebfb181447aa0f80
Panwar <VPanwar2@ad.harman.com>
2019-12-02 11:11:18 +0530

    added animations

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script>
+	import {slide, fade ,blur} from "svelte/transition" 
 	import Surname from "./surname.svelte";
 	import TagLineGenerator from "./tagLineGenerator.svelte";
 	import DemoCompo from "./DemoCompo.svelte";
@@ -6,7 +7,26 @@
 	let vineet = "vineet";
 	let stringToShow = "";
 	let htmlToShow = "";
+	let bindChecked = false;
 </script>
+	<form>
+		<div class="username">
+			<label for="username">Enter your username:</label>
+			<input type="text" name="username" placeholder="username here" />
+		</div>
+		<div class="password">
+			<label for="password">Enter your password:</label>
+			<input type="password" name="password" />
+		</div>
+		<label>
+			<input type="checkbox" bind:checked={bindChecked} />
+			do you wanna login ?
+		</label>
+	</form>
+
+	{#if bindChecked}
+		<h1 transition:blur={{duration:1200,delay:100}}>okay,getting you in </h1>
+	{/if}
 
 	<Box>
 	<h1 slot="header">Hello</h1>
